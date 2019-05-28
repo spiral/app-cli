8f270464b09642f19b1b0adf60cf78c8a8d72dc9
Wolfy-J <wolfy.jd@gmail.com>
2019-05-28 20:49:11 +0300

    less deps, smaller app

--- app/src/App.php
+++ app/src/App.php
@@ -10,7 +10,6 @@ declare(strict_types=1);
 namespace App;
 
 use Spiral\Bootloader;
-use Spiral\DotEnv\Bootloader as DotEnv;
 use Spiral\Framework\Kernel;
 
 class App extends Kernel
@@ -20,15 +19,7 @@ class App extends Kernel
      * within system container on application start.
      */
     protected const LOAD = [
-        // Environment configuration
-        DotEnv\DotenvBootloader::class,
         Bootloader\DebugBootloader::class,
         Bootloader\CommandBootloader::class
-    ];
-
-    /*
-     * Application specific services and extensions.
-     */
-    protected const APP = [
     ];
 }
